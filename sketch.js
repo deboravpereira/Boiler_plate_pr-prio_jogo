82e6fce419eefcdffa4b569e5e4cea26f784ce73
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2023-05-16 16:47:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,28 @@
-
+//Crie aqui as variáveis dos personagens e cenário
 
 function preload(){
   //Carregar imagens e sons
+  
+  //Carregar imagem variavel = loadImage("nome-do-arquivo.png")
+  
+  //Carregar animação variavel = loadAnimation("nome-do-arquivo.png","nome-do-arquivo1.png","nome-do-arquivo2.png")
+  
+  //Carregar sons  variavel = loadSound("nome-do-arquivo.mp3");
+  
 }
 
 function setup(){
   //Configurar jogo
+  createCanvas(windowWidth,windowHeight);
+  
+  //Crie sprites e adicione suas imagens
 
  
 }
 
 function draw() {
+  background("white");
 
-  drawSprites()
+  drawSprites();
     
 }
